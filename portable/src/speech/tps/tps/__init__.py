0a445d16d8ba0b7d3f63cd2cae1b074b0bab70fa
Vladislav <maksic1996@yandex.ru>
2023-09-05 11:54:45 +0300

    fix ntlk ssl bug

--- portable/src/speech/tps/tps/__init__.py
+++ portable/src/speech/tps/tps/__init__.py
@@ -1,8 +1,20 @@
-import nltk
+import os
+from backend.folders import RTVC_VOICE_FOLDER
+from backend.download import download_model, unzip, check_download_size
+
+punkt_url = "https://raw.githubusercontent.com/nltk/nltk_data/gh-pages/packages/tokenizers/punkt.zip"
+tokenizers_punkt_path = os.path.join(RTVC_VOICE_FOLDER, "tokenizers")
+
 try:
-    nltk.data.find("tokenizers/punkt")
-except LookupError:
-    nltk.download("punkt")
+    if not os.path.exists(os.path.dirname(tokenizers_punkt_path)):
+        os.makedirs(os.path.dirname(tokenizers_punkt_path))
+    if not os.path.exists(os.path.join(tokenizers_punkt_path, "punkt")):
+        download_model(os.path.join(tokenizers_punkt_path, "punkt.zip"), punkt_url)
+        unzip(os.path.join(tokenizers_punkt_path, 'punkt.zip'), tokenizers_punkt_path)
+    else:
+        check_download_size(os.path.join(tokenizers_punkt_path, "punkt.zip"), punkt_url)
+except Exception as err:
+    print(f"Error during download NLTK {err}")
 
 from tps.handler import Handler, get_symbols_length
 from tps.utils import cleaners, load_dict, save_dict, prob2bool, split_to_tokens
