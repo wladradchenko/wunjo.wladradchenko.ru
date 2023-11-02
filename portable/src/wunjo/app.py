650f9316dca136555ef14a3245e6ebeb885ccc3b
Vladislav <maksic1996@yandex.ru>
2023-11-02 18:47:26 +0300

    cgeck debug option

--- portable/src/wunjo/app.py
+++ portable/src/wunjo/app.py
@@ -34,7 +34,7 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
 
-app.config['DEBUG'] = True
+app.config['DEBUG'] = False
 app.config['SYNTHESIZE_STATUS'] = {"status_code": 200, "message": ""}
 app.config['SYNTHESIZE_RESULT'] = []
 app.config['SEGMENT_ANYTHING_MASK_PREVIEW_RESULT'] = {}  # get segment result
