8608fade28c5fa978814cdbc12b4167a8a2e277f
Vladislav <maksic1996@yandex.ru>
2023-11-10 13:40:48 +0300

    fix bug with torch requires for macos

--- portable/src/wunjo/app.py
+++ portable/src/wunjo/app.py
@@ -1016,7 +1016,7 @@ if not app.config['DEBUG']:
         combined_logs = console_stdout.logs + console_stderr.logs
         combined_logs.sort(key=lambda x: x[0])  # Sort by timestamp
 
-        # Remove bytes log
+        # Covert bytes log
         combined_logs = [(log[0], log[1].decode('utf-8')) if isinstance(log[1], bytes) else log for log in combined_logs]
 
         # Filter out unwanted phrases and extract log messages
