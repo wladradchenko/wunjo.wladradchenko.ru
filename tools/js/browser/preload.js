aa2e693d8701b75bb4d12353295221464007a5a4
Vladislav <maksic1996@yandex.ru>
2025-05-13 10:03:23 +0300

    added SignPath

---
+++ tools/js/browser/preload.js
@@ -0,0 +1,19 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const type of ["chrome", "node", "electron"]) {
+    replaceText(`${type}-version`, process.versions[type]);
+  }
+});
+
+// Expose the ipcRenderer to the renderer process
+contextBridge.exposeInMainWorld("electron", {
+  send: (channel, data) => ipcRenderer.send(channel, data),
+  receive: (channel, func) =>
+    ipcRenderer.on(channel, (event, ...args) => func(...args)),
+});
